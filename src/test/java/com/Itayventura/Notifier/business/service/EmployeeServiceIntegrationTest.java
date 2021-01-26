b6e31f00078b22702ab65bba936c7cb488746eb7
itayventura <ventura.itay@gmail.com>
2021-01-26 22:32:24 +0200

    added tests for get employee, get not exisiting employee, delete employee. furthermore, made each test independent. that means that if i add/update/delete data then i return it to the initial state before the test finishes executing so that each test can run independently

--- src/test/java/com/Itayventura/Notifier/business/service/EmployeeServiceIntegrationTest.java
+++ src/test/java/com/Itayventura/Notifier/business/service/EmployeeServiceIntegrationTest.java
@@ -2,6 +2,8 @@ package com.Itayventura.Notifier.business.service;
 
 import com.Itayventura.Notifier.data.entity.Employee;
 import com.Itayventura.Notifier.data.entity.Team;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,80 +20,137 @@ public class EmployeeServiceIntegrationTest {
     @Autowired
     private EmployeeService employeeService;
 
-    @Test
-    public void testAddEmployee(){
+    private Employee employee;
+
+    @Before
+    public void setUp(){
         Team team = new Team();
         team.setTeamId(1);
         team.setTeamName("sw1");
         team.setDepartment("R&D");
 
-        Employee employee = new Employee();
+        employee = new Employee();
+        employee.setEmployeeId(4);
         employee.setTeam(team);
-        employee.setEmployeeId(1);
         employee.setRoll("software developer");
         employee.setLastName("Levi");
         employee.setEmailAddress("a@a.com");
         employee.setFirstName("David");
+    }
 
+    @Test
+    public void testAddEmployee(){
         Employee newEmployee = employeeService.addEmployee(employee);
+        assertNotNull(newEmployee);
+        employee.setEmployeeId(newEmployee.getEmployeeId());
 
         assertNotNull(newEmployee);
-        assertEquals(1, newEmployee.getEmployeeId());
-        assertEquals("David", newEmployee.getFirstName());
-        assertEquals("Levi", newEmployee.getLastName());
-        assertEquals("a@a.com", newEmployee.getEmailAddress());
-        assertEquals("software developer", newEmployee.getRoll());
-        assertNotNull(newEmployee.getTeam());
+        assertEquals(employee.getFirstName(), newEmployee.getFirstName());
+        assertEquals(employee.getLastName(), newEmployee.getLastName());
+        assertEquals(employee.getEmailAddress(), newEmployee.getEmailAddress());
+        assertEquals(employee.getRoll(), newEmployee.getRoll());
+        assertEquals(employee.getTeam().getTeamId(), newEmployee.getTeam().getTeamId());
+        assertEquals(employee.getTeam().getDepartment(), newEmployee.getTeam().getDepartment());
+        assertEquals(employee.getTeam().getName(), newEmployee.getTeam().getName());
+
+        employeeService.deleteEmployee(employee);
+
+        newEmployee = employeeService.getEmployee(employee.getFirstName(), employee.getLastName());
+        assertNull(newEmployee);
     }
 
     @Test (expected = org.springframework.dao.InvalidDataAccessApiUsageException.class)
     public void testAddEmployeeToNotExistingTeam(){
-        Employee employee = new Employee();
-        employee.setTeam(new Team());
-        employee.setEmployeeId(1);
-        employee.setRoll("software developer");
-        employee.setLastName("Levi");
-        employee.setEmailAddress("a@a.com");
-        employee.setFirstName("David");
-        Employee newEmployee = employeeService.addEmployee(employee);
+        Team team = employee.getTeam();
+        try{
+            employee.setTeam(new Team());
+            Employee newEmployee = employeeService.addEmployee(employee);
+            assertNotNull(newEmployee);
+            employee.setEmployeeId(newEmployee.getEmployeeId());
+
+            assertNotNull(newEmployee.getTeam());
+
+        } finally {
+            employee.setTeam(team);
+            assertEquals(team, employee.getTeam());
+        }
 
     }
 
     @Test
     public void testUpdateEmployee(){
-        Team team = new Team();
-        team.setTeamId(1);
-        team.setTeamName("sw1");
-        team.setDepartment("R&D");
+        Employee newEmployee = employeeService.addEmployee(employee);
+        assertNotNull(newEmployee);
+        employee.setEmployeeId(newEmployee.getEmployeeId());
 
-        Employee employee = new Employee();
-        employee.setEmployeeId(1);
-        employee.setFirstName("Not Itay");
-        employee.setLastName("Not Ventura");
-        employee.setRoll("Not Software Developer");
-        employee.setTeam(team);
-        employee.setEmailAddress("Not a@a.com");
-
-        Employee newEmployee = employeeService.updateEmployee(employee);
+        newEmployee.setFirstName("Not " + employee.getFirstName());
+        newEmployee.setLastName("Not " + employee.getLastName());
+        newEmployee = employeeService.updateEmployee(newEmployee);
 
         assertNotNull(newEmployee);
-        assertEquals("Not Itay", newEmployee.getFirstName());
+        assertEquals("Not " + employee.getFirstName(), newEmployee.getFirstName());
+        assertEquals("Not " + employee.getLastName(), newEmployee.getLastName());
 
-
-
+        employeeService.deleteEmployee(newEmployee);
+        newEmployee = employeeService.getEmployee(newEmployee.getFirstName(), newEmployee.getLastName());
+        assertNull(newEmployee);
     }
 
     @Test(expected = org.springframework.dao.DataIntegrityViolationException.class)
     public void testUpdateEmployeeWithMissingAttributes(){
-        Employee employee = new Employee();
-        employee.setEmployeeId(1);
-        employee.setFirstName("Not Itay");
-
-        Employee newEmployee = employeeService.updateEmployee(employee);
-
+        Employee newEmployee = employeeService.addEmployee(employee);
         assertNotNull(newEmployee);
-        assertEquals("Not Itay", newEmployee.getFirstName());
+        employee.setEmployeeId(newEmployee.getEmployeeId());
 
+        String firstName = newEmployee.getFirstName();
+        try{
+            newEmployee.setFirstName(null);
+            newEmployee = employeeService.updateEmployee(newEmployee);
+
+        } finally {
+            newEmployee = employeeService.getEmployee(employee.getFirstName(), employee.getLastName());
+            assertNotNull(newEmployee);
+            assertNotNull(newEmployee.getFirstName());
+            employeeService.deleteEmployee(newEmployee);
+            newEmployee = employeeService.getEmployee(employee.getFirstName(), employee.getLastName());
+            assertNull(newEmployee);
+        }
     }
+
+    @Test
+    public void testDeleteEmployee(){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        assertNotNull(newEmployee);
+        employee.setEmployeeId(newEmployee.getEmployeeId());
+        employeeService.deleteEmployee(employee);
+        newEmployee = employeeService.getEmployee(employee.getFirstName(), employee.getLastName());
+        assertNull(newEmployee);
+    }
+
+    @Test
+    public void getEmployee(){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        assertNotNull(newEmployee);
+        employee.setEmployeeId(newEmployee.getEmployeeId());
+        newEmployee = employeeService.getEmployee(employee.getFirstName(), employee.getLastName());
+        assertNotNull(newEmployee);
+        assertEquals(employee.getRoll(), newEmployee.getRoll());
+        assertEquals(employee.getFirstName(), newEmployee.getFirstName());
+        assertEquals(employee.getEmailAddress(), newEmployee.getEmailAddress());
+        assertEquals(employee.getLastName(), newEmployee.getLastName());
+        assertEquals(employee.getTeam().getName(), newEmployee.getTeam().getName());
+        assertEquals(employee.getTeam().getDepartment(), newEmployee.getTeam().getDepartment());
+
+        employeeService.deleteEmployee(newEmployee);
+        newEmployee = employeeService.getEmployee(employee.getFirstName(), employee.getLastName());
+        assertNull(newEmployee);
+    }
+
+    @Test
+    public void getNotExistingEmployee(){
+        Employee newEmployee = employeeService.getEmployee("not", "exist");
+        assertNull(newEmployee);
+    }
+
 
 }
