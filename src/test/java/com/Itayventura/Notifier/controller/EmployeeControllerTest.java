4ef8fac2ad478d123302ec0065ed37bc5757cc96
itayventura <ventura.itay@gmail.com>
2021-01-31 15:37:42 +0200

    catching all exceptions with anotation

--- src/test/java/com/Itayventura/Notifier/controller/EmployeeControllerTest.java
+++ src/test/java/com/Itayventura/Notifier/controller/EmployeeControllerTest.java
@@ -8,30 +8,37 @@ import com.Itayventura.Notifier.payroll.EmployeeModelAssembler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(EmployeeController.class)
+@AutoConfigureMockMvc
 public class EmployeeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
 
     @MockBean
     private EmployeeService employeeService;
@@ -80,7 +87,6 @@ public class EmployeeControllerTest {
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().is2xxSuccessful());
     }
-
 
     @Test
     public void testAddEmptyEmployee() throws Exception {
