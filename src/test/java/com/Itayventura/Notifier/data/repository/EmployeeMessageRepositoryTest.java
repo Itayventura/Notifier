959b30a06a2e35eb6b07e94081aa2e1f7faf8835
itayventura <ventura.itay@gmail.com>
2021-02-01 17:56:14 +0200

    remove unused imports

--- src/test/java/com/Itayventura/Notifier/data/repository/EmployeeMessageRepositoryTest.java
+++ src/test/java/com/Itayventura/Notifier/data/repository/EmployeeMessageRepositoryTest.java
@@ -13,12 +13,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
