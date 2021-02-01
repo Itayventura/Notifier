9f9a0a60c59e91c4d2aa1a5613b7a071719e8c78
itayventura <ventura.itay@gmail.com>
2021-02-01 14:03:16 +0200

    adding test to team model assembler

---
+++ src/test/java/com/Itayventura/Notifier/payroll/TeamModelAssemblerTest.java
@@ -0,0 +1,47 @@
+package com.Itayventura.Notifier.payroll;
+
+import com.Itayventura.Notifier.data.entity.Team;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.Links;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TeamModelAssemblerTest {
+
+    private TeamModelAssembler assembler;
+    private Team team;
+
+    @Before
+    public void setUp() {
+        team = new Team(0, "r", "r");
+        assembler = new TeamModelAssembler();
+    }
+
+    @Test
+    public void toModel() {
+        EntityModel<Team> entityModel = assembler.toModel(team);
+        Team newTeam = entityModel.getContent();
+        assertEquals(team, newTeam);
+
+        Links links = entityModel.getLinks();
+
+        Optional<Link> opt = links.getLink("self");
+        assertTrue(opt.isPresent());
+
+        Link self = opt.get();
+        assertEquals("/teams/"+team.getTeamId(), self.getHref());
+
+        opt = links.getLink("teams");
+        assertTrue(opt.isPresent());
+
+        Link all = opt.get();
+        assertEquals("/teams", all.getHref());
+    }
+}
